3ec2366119b3abbb7e5c48433e9e197f689d7625
vmkvsite <vikronja@vsite.hr>
2024-12-25 23:20:45 +0100

    Gotova vjezba 5

--- EmployeeListApp/Program.cs
+++ EmployeeListApp/Program.cs
@@ -1,6 +1,8 @@
 using EmployeeListApp.Data;
+using EmployeeListApp.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<EmployeesService>();
+
+
 
 var app = builder.Build();
 
