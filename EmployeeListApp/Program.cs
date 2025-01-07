61a12cf5cc4f4d27aabcc1715d70bdbe8cd59ed1
ivan0301 <ivangrgic0301@gmail.com>
2025-01-07 20:26:35 +0100

    rijesen labos

--- EmployeeListApp/Program.cs
+++ EmployeeListApp/Program.cs
@@ -1,21 +1,23 @@
 using EmployeeListApp.Data;
+using EmployeeListApp.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<EmployeesService>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
---
+++ EmployeeListApp/Services/EmployeesService.cs
@@ -0,0 +1,62 @@
+﻿using EmployeeListApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeListApp.Services
+{
+    public class EmployeesService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            return await _context.Employees.ToListAsync();  
+        }
+
+        public async Task<Employee> GetEmployeeByIdAsync(string id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+
+        public async Task<Employee> InsertEmployeeAsync(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        public async Task<Employee> UpdateEmployeeAsync(string id, Employee employee)
+        {
+            var existingEmployee = await _context.Employees.FindAsync(id);
+            if (existingEmployee == null)
+                return null;
+            existingEmployee.FullName = employee.FullName;
+            existingEmployee.Department = employee.Department;
+            existingEmployee.Salary = employee.Salary;
+
+            _context.Employees.Update(existingEmployee);
+            await _context.SaveChangesAsync();
+            return existingEmployee;
+        }
+
+        public async Task<Employee> DeleteEmployeeAsync(string id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+                return null;
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        private bool EmployeeExists(string id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
