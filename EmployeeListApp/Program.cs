023c06c944abea6f6ba70ec67c5f50336ccaab9a
Filip Maretić <fimareti@vsite.hr>
2025-01-07 20:07:10 +0100

    rijeseno

--- EmployeeListApp/Program.cs
+++ EmployeeListApp/Program.cs
@@ -1,6 +1,8 @@
 using EmployeeListApp.Data;
+using EmployeeListApp.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
+
+builder.Services.AddScoped<EmployeesService>();
+
 
 var app = builder.Build();
 
---
+++ EmployeeListApp/Services/EmployeesService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeListApp.Data;
+
+namespace EmployeeListApp.Services
+{
+    public class EmployeesService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Employee>> GetAllEmployeesAsync()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> AddEmployeeAsync(Employee employee)
+        {
+            employee.Id = Guid.NewGuid().ToString();
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        public async Task<bool> DeleteEmployeeAsync(string id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null) return false;
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
