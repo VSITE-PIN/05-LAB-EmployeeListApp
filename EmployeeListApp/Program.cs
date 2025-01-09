30922c0c7539b5b8daf2daa03a30c3be2f25ed14
veronikamataija1 <vemataija@vsite.hr>
2025-01-09 16:32:05 +0100

    Labos 5.

--- EmployeeListApp/Program.cs
+++ EmployeeListApp/Program.cs
@@ -1,12 +1,17 @@
 using EmployeeListApp.Data;
+using EmployeeListApp.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<EmployeesService>();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
---
+++ EmployeeListApp/Services/EmployeesService.cs
@@ -0,0 +1,52 @@
+﻿using EmployeeListApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeListApp.Services
+{
+    public class EmployeesService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Dohvat svih zaposlenika
+        public async Task<List<Employee>> GetAllEmployeesAsync()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        // Dohvat jednog zaposlenika po ID-u
+        public async Task<Employee?> GetEmployeeByIdAsync(string id)
+        {
+            return await _context.Employees.FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        // Dodavanje novog zaposlenika
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        // Ažuriranje zaposlenika
+        public async Task UpdateEmployeeAsync(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        // Brisanje zaposlenika
+        public async Task DeleteEmployeeAsync(string id)
+        {
+            var employee = await _context.Employees.FirstOrDefaultAsync(e => e.Id == id);
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
