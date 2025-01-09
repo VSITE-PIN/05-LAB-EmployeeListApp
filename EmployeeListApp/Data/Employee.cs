5c5a9b41fdb4a321f99c214353583bc54a36202d
Juled Halili <juhalili@vsite.hr>
2025-01-09 19:49:19 +0100

    Dodani nuget paketi

---
+++ EmployeeListApp/Data/Employee.cs
@@ -0,0 +1,7 @@
+﻿public class Employee
+{
+    public required string Id { get; set; }
+    public required string FullName { get; set; }
+    public required string Department { get; set; }
+    public required decimal Salary { get; set; }
+}
