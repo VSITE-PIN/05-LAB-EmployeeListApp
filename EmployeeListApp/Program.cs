5fea8d13a5aacb3ba71434ee2bc156712cbdc5e2
Juled Halili <juhalili@vsite.hr>
2025-01-09 20:01:31 +0100

    Rijeseno

--- EmployeeListApp/Program.cs
+++ EmployeeListApp/Program.cs
@@ -1,6 +1,7 @@
 using EmployeeListApp.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddScoped<EmployeesService>();
 
 var app = builder.Build();
 
---
+++ EmployeeListApp/Services/EmployeesService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class EmployeesService
+{
+    private readonly AppDbContext _context;
+
+    public EmployeesService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddEmployee(Employee employee)
+    {
+        _context.Employees.Add(employee);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateEmployee(Employee employee)
+    {
+        _context.Employees.Update(employee);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<Employee>> GetAllEmployees()
+    {
+        return await _context.Employees.ToListAsync();
+    }
+
+    public async Task<Employee> GetEmployeeById(string id)
+    {
+        return await _context.Employees.FirstOrDefaultAsync(e => e.Id == id);
+    }
+
+    public async Task DeleteEmployee(string id)
+    {
+        var employee = await _context.Employees.FindAsync(id);
+        if (employee != null)
+        {
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
