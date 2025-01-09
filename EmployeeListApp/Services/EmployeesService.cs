fb692154396f148aa9ccf5228008ed5ae3bc393b
Marija Čuljak <marculjak@vsite.hr>
2025-01-09 17:41:58 +0100

    labos9.1.MarijaČuljak

---
+++ EmployeeListApp/Services/EmployeesService.cs
@@ -0,0 +1,41 @@
+﻿using EmployeeListApp.Data;
+
+namespace EmployeeListApp.Services
+{
+    public class EmployeesService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Employee>> GetAllEmployeesAsync() => await _context.Employees.ToListAsync();
+
+        public async Task<Employee> GetEmployeeByIdAsync(string id) => await _context.Employees.FindAsync(id);
+
+        public async Task AddEmployeeAsync(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateEmployeeAsync(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteEmployeeAsync(string id)
+        {
+            var employee = await GetEmployeeByIdAsync(id);
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
+}
