d7f838dd00f92d9dc6bbca9dc6cfd7063966e104
krvukova <krvukova@vsite.hr>
2025-01-09 19:22:05 +0100

    prvidio

---
+++ EmployeeListApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeListApp.Pages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeListApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+        base(options){ }
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
