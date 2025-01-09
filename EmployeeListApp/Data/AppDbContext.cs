815aafd1a942c53df6be78ab6fec8b971d8103e2
Franko Junakovic <franko.junakovic@vsite.hr>
2025-01-09 19:29:22 +0100

    LAB5 Zavrsen

---
+++ EmployeeListApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeListApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+       base(options)
+        { }
+    }
+}
