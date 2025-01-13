2df8ee8bad8e987e7eb2fd87db9d3812005d355f
Iva Brletić <ivbrleti@vsite.hr>
2025-01-13 23:43:07 +0100

    vj 5

--- EmployeeListApp/Program.cs
+++ EmployeeListApp/Program.cs
@@ -1,13 +1,16 @@
 using EmployeeListApp.Data;
+using EmployeeListApp.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<EmployeesService>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnString") ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
 
 var app = builder.Build();
 
